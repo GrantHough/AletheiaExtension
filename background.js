487f5c9533f96c8c79a1f612f6ebb149a500862e
YanisHerne <y.herne@icloud.com>
2023-10-01 11:25:08 -0700

    Basic server contact working

---
+++ background.js
@@ -0,0 +1,34 @@
+// Service worker
+const checkURL = "https://aletheianode-ez7hynivba-uc.a.run.app/"
+
+function verifyText(raw_text, request_source, popup_response) { // Universal checking function
+    console.log(raw_text);
+    console.log(request_source);
+    var response_text;
+    fetch(checkURL, {
+        method: 'POST',
+        body: JSON.stringify({
+            text: raw_text, // Gather the text input from the DOM
+        }),
+        headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'}
+    })
+    .then(response => {
+        if (response.ok) { // Only decode message if status ok, otherwise throw error
+            return response.json()
+        }
+        else {
+            throw new Error("Error contacting the server: Error Code " + response.status);
+        }
+    })
+    .then(data => {
+        console.log(data.message)
+        response_text = data.message;
+        return response_text
+    })
+    .catch((error) => {
+        console.error(error);
+        response_text = error.message;
+        console.log("response_text:" + response_text)
+        return response_text
+    });
+}
---
+++ popup.js
@@ -0,0 +1,54 @@
+/* This javascript file runs the popup generated when the chrome extension 
+is clicked in the top right corner of the browser window.
+See popup.css and popup.html*/
+
+// const checkURL = "https://tempservergy-b7d8de4f1efb.herokuapp.com/check"
+const checkURL = "https://grantyanistempserver-kphgaowlda-uw.a.run.app/check"
+
+// Buttons
+const verify_button = document.getElementById("verify_button");
+const settings_button = document.getElementById("settings_button");
+const return_button = document.getElementById("return_button");
+
+// Inputs / Outputs
+const raw_text = document.getElementById("raw_text");
+const return_text = document.getElementById("return_text");
+
+document.addEventListener("DOMContentLoaded", function(event) { // Make sure that popup DOM is loaded to prevent possible strange bugs
+
+    verify_button.addEventListener("click", verify_function);
+    return_button.addEventListener("click", return_function);
+
+    function verify_function() {
+        verify_button.removeEventListener("click", verify_function); // Prevent the button from being spammed by unbinding callback
+        hide(raw_text);
+        show(return_text);
+        (async () => {
+            const response = await chrome.runtime.sendMessage({action: "check", raw_text: raw_text});
+            set_return_text(response);
+            console.log(response);
+          })();
+        verify_button.addEventListener("click", verify_function); // Rebind callback so the button can be clicked again
+        hide(verify_button);
+        show(return_button);
+    }
+
+    function return_function() {
+        hide(return_text);
+        hide(return_button);
+        show(raw_text);
+        show(verify_button);
+    }
+
+    function set_return_text(message) { // Utility function to improve readability
+        return_text.innerHTML = message;
+    }
+
+    function show(element) { // Utility function to show given element
+        element.style.display = "block";
+    }
+
+    function hide (element) { // Utility function to hide given element
+        element.style.display = "none";
+    }
+});
