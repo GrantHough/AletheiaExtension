7cbdc197bdf0867d8bf1461ae725520220544d11
YanisHerne <y.herne@icloud.com>
2023-10-01 13:45:20 -0700

    Working on the 3 functions

--- background.js
+++ background.js
@@ -1,20 +1,28 @@
 // Service worker
 const checkURL = "https://aletheianode-ez7hynivba-uc.a.run.app"
+//let activeURL;
 
 chrome.contextMenus.removeAll(function() {
     chrome.contextMenus.create({
      id: "1",
-     title: "VerifyAI",
+     title: "Check with Aletheia",
      contexts:["selection"],  // ContextType
      type: "normal"
     }); })
 
 chrome.contextMenus.onClicked.addListener(verifyHelper); // Listener for right-click context menu
+//chrome.tabs.onUpdated.addListener(getActiveUrl);
 
 chrome.runtime.onMessage.addListener( // Listener for popup in top right
     function(request, sender, sendResponse) {
         if (request.action == "check") {
             const response = verifyText(request.raw_text, "popup", sendResponse); // Call verify function, add sendResponse as function parameter
+        }
+        if (request.action == "bias") {
+            const response = articleBias(sendResponse);
+        }
+        if (request.action == "summarize") {
+            const response = articleSummary(sendResponse);
         }
         return true;
     }
@@ -25,13 +33,13 @@ function verifyHelper(info) { // Unwraps the selected text from right-click cont
 }
 
 function verifyText(raw_text, request_source, popup_response) { // Universal checking function
-    console.log(raw_text);
+    console.log("The raw text prompt is:" + raw_text);
     console.log(request_source);
     var response_text;
     fetch(checkURL + "/validate", {
         method: 'POST',
         body: JSON.stringify({
-            text: raw_text, // Gather the text input from the DOM
+            text: raw_text // Gather the text input from the DOM
         }),
         mode: 'cors',
         headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'}
@@ -56,26 +64,96 @@ function verifyText(raw_text, request_source, popup_response) { // Universal che
         return response_text
     })
     .then(response_text => {
-        if (request_source == "contextMenu") {
-            (async () => {
-                const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true}); // Find current tab
-                const response = await chrome.tabs.sendMessage(tab.id, {response_text: response_text}, function(response) {
-                    if (chrome.runtime.lastError.message === "Could not establish connection. Receiving end does not exist.") {
-                        chrome.scripting.executeScript({
-                            target: { tabId: tab.id },
-                            files: ['content.js']
-                        });
-                        chrome.tabs.sendMessage(tab.id, {response_text: response_text});
-                    }
-                });
-            })();
-        }
-        else {
-            console.log("response_text:" + response_text)
-            if(response_text) {
                 popup_response(response_text);
-                console.log("Popup response sent")
-            }
-        }
+                console.log("Response sent to popup")
+
     });
+}
+
+function articleBias(sendResponse) {
+    const getActiveUrl = (tabid, changeInfo, tab) => {
+        const url = changeInfo.url;
+        // url is likely to be empty, and filter chrome:// and about:// URLs
+        if (!url || ['chrome://', 'about://'].some(p => url.startsWith(p))) return;
+        // filtering is not an active tab
+        if (!tab.active) return;
+        // the url address you need
+        console.log(url);
+        fetch(checkURL + "/articlebias", {
+            method: 'POST',
+            body: JSON.stringify({
+                url: url
+            }),
+            mode: 'cors',
+            headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'}
+        })
+        .then(response => {
+            if (response.ok) { // Only decode message if status ok, otherwise throw error
+                return response.json()
+            }
+            else {
+                throw new Error("Error contacting the server: Error Code " + response.status);
+            }
+        })
+        .then(data => {
+            console.log(data.message)
+            response_text = data.message;
+            return response_text
+        })
+        .catch((error) => {
+            console.error(error);
+            response_text = error.message;
+            console.log("response_text:" + response_text)
+            return response_text
+        })
+        .then(response_text => {
+                    sendResponse(response_text);
+                    console.log("Response sent to popup")
+    
+        });
+    }
+}
+
+function articleSummary(sendResponse) {
+    const getActiveUrl = (tabid, changeInfo, tab) => {
+        const url = changeInfo.url;
+        // url is likely to be empty, and filter chrome:// and about:// URLs
+        if (!url || ['chrome://', 'about://'].some(p => url.startsWith(p))) return;
+        // filtering is not an active tab
+        if (!tab.active) return;
+        // the url address you need
+        console.log(url);
+        fetch(checkURL + "/summarize", {
+            method: 'POST',
+            body: JSON.stringify({
+                url: url
+            }),
+            mode: 'cors',
+            headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'}
+        })
+        .then(response => {
+            if (response.ok) { // Only decode message if status ok, otherwise throw error
+                return response.json()
+            }
+            else {
+                throw new Error("Error contacting the server: Error Code " + response.status);
+            }
+        })
+        .then(data => {
+            console.log(data.message)
+            response_text = data.message;
+            return response_text
+        })
+        .catch((error) => {
+            console.error(error);
+            response_text = error.message;
+            console.log("response_text:" + response_text)
+            return response_text
+        })
+        .then(response_text => {
+                    sendResponse(response_text);
+                    console.log("Response sent to popup")
+    
+        });
+    }
 }
--- popup.js
+++ popup.js
@@ -4,6 +4,8 @@ See popup.css and popup.html*/
 
 // Buttons
 const verify_button = document.getElementById("verify_button");
+const summary_button = document.getElementById("summary_button");
+const bias_button = document.getElementById("bias_button");
 const settings_button = document.getElementById("settings_button");
 const return_button = document.getElementById("return_button");
 
@@ -11,23 +13,30 @@ const return_button = document.getElementById("return_button");
 const raw_text = document.getElementById("raw_text");
 const return_text = document.getElementById("return_text");
 
+const PLACEHOLDER = " Status Unknown ";
+
 document.addEventListener("DOMContentLoaded", function(event) { // Make sure that popup DOM is loaded to prevent possible strange bugs
 
     verify_button.addEventListener("click", verify_function);
     return_button.addEventListener("click", return_function);
+    summary_button.addEventListener("click", articleSummary);
+    bias_button.addEventListener("click", articleBias);
 
     function verify_function() {
+        return_text.innerHTML = PLACEHOLDER;
         verify_button.removeEventListener("click", verify_function); // Prevent the button from being spammed by unbinding callback
         hide(raw_text);
+        hide(summary_button);
+        hide(bias_button);
+        hide(verify_button);
         show(return_text);
+        show(return_button);
         (async () => {
-            const response = await chrome.runtime.sendMessage({action: "check", raw_text: raw_text});
+            const response = await chrome.runtime.sendMessage({action: "check", raw_text: raw_text.value});
             set_return_text(response);
             console.log(response);
           })();
         verify_button.addEventListener("click", verify_function); // Rebind callback so the button can be clicked again
-        hide(verify_button);
-        show(return_button);
     }
 
     function return_function() {
@@ -35,6 +44,8 @@ document.addEventListener("DOMContentLoaded", function(event) { // Make sure tha
         hide(return_button);
         show(raw_text);
         show(verify_button);
+        show(summary_button)
+        show(bias_button);
     }
 
     function set_return_text(message) { // Utility function to improve readability
@@ -47,5 +58,36 @@ document.addEventListener("DOMContentLoaded", function(event) { // Make sure tha
 
     function hide (element) { // Utility function to hide given element
         element.style.display = "none";
+    }
+    function articleBias() {
+        return_text.innerHTML = PLACEHOLDER;
+        verify_button.removeEventListener("click", verify_function); // Prevent the button from being spammed by unbinding callback
+        hide(raw_text);
+        hide(summary_button);
+        hide(bias_button);
+        hide(verify_button);
+        show(return_text);
+        show(return_button);
+        (async () => {
+            const response = await chrome.runtime.sendMessage({action: "bias"});
+            set_return_text(response);
+            console.log(response);
+          })();
+    }
+    function articleSummary() {
+        return_text.innerHTML = PLACEHOLDER;
+        verify_button.removeEventListener("click", verify_function); // Prevent the button from being spammed by unbinding callback
+        hide(raw_text);
+        hide(summary_button);
+        hide(bias_button);
+        hide(verify_button);
+        show(return_text);
+        show(return_button);
+        (async () => {
+            const response = await chrome.runtime.sendMessage({action: "summarize"});
+            set_return_text(response);
+            console.log(response);
+          })();
+        verify_button.addEventListener("click", verify_function); // Rebind callback so the button can be clicked again
     }
 });
